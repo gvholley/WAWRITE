954e2c30f489cbced61bbb33927ea9bc3888b6df
Grant Holley <gvholley@gmail.com>
2020-06-18 12:58:13 +0900

    Added payment controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_17_060409) do
+ActiveRecord::Schema.define(version: 2020_06_18_032019) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,19 @@ ActiveRecord::Schema.define(version: 2020_06_17_060409) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "orders", force: :cascade do |t|
+    t.string "state"
+    t.string "product_sku"
+    t.integer "amount_cents", default: 0, null: false
+    t.string "checkout_session_id"
+    t.bigint "user_id", null: false
+    t.bigint "product_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["product_id"], name: "index_orders_on_product_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "sku"
     t.string "name"
@@ -28,6 +41,7 @@ ActiveRecord::Schema.define(version: 2020_06_17_060409) do
     t.string "photo_url"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "price_cents", default: 0, null: false
     t.index ["category_id"], name: "index_products_on_category_id"
   end
 
@@ -43,5 +57,7 @@ ActiveRecord::Schema.define(version: 2020_06_17_060409) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "orders", "products"
+  add_foreign_key "orders", "users"
   add_foreign_key "products", "categories"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Product.destroy_all if Rails.env.development?
+Category.destroy_all
+
+puts 'Creating categories...'
+diary = Category.create!(name: 'diary')
+pen = Category.create!(name: 'pen')
+
+puts 'Creating stationery...'
+Product.create!(price_cents: 3499, sku: 'hobonichi', name: 'Hobonichi Techo', category: diary, photo_url: 'https://www.1101.com/store/techo/2020/images/all_about/original_index/img_01.jpg')
+Product.create!(price_cents: 499, sku: 'stylefit', name: 'Uni Style Fit', category: pen, photo_url: 'https://images-na.ssl-images-amazon.com/images/I/91pvFXzZclL._AC_SL1500_.jpg')
+Product.create!(price_cents: 1999, sku: 'apica', name: 'Apica Three Year Diary', category: diary, photo_url: 'https://image.rakuten.co.jp/horiman/cabinet/item-p3/d303.jpg')
+puts 'Finished!'
