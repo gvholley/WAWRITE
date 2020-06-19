b3588f01a02aab19758d3a99e277e90499aead56
Grant Holley <gvholley@gmail.com>
2020-06-19 18:02:24 +0900

    Changed layout and added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,11 +3,15 @@ Product.destroy_all if Rails.env.development?
 Category.destroy_all
 
 puts 'Creating categories...'
-diary = Category.create!(name: 'diary')
-pen = Category.create!(name: 'pen')
+diary = Category.create!(name: 'Diary')
+pen = Category.create!(name: 'Pen')
+tape = Category.create!(name: 'Tape')
 
 puts 'Creating stationery...'
-Product.create!(price_cents: 3499, sku: 'hobonichi', name: 'Hobonichi Techo', category: diary, photo_url: 'https://www.1101.com/store/techo/2020/images/all_about/original_index/img_01.jpg')
+Product.create!(price_cents: 3499, sku: 'hobonichi', name: 'Hobonichi Techo', category: diary, photo_url: 'https://www.1101.com/store/techo/2019/images/all_about/planner_about04/zoom_01.jpg')
 Product.create!(price_cents: 499, sku: 'stylefit', name: 'Uni Style Fit', category: pen, photo_url: 'https://images-na.ssl-images-amazon.com/images/I/91pvFXzZclL._AC_SL1500_.jpg')
-Product.create!(price_cents: 1999, sku: 'apica', name: 'Apica Three Year Diary', category: diary, photo_url: 'https://image.rakuten.co.jp/horiman/cabinet/item-p3/d303.jpg')
+Product.create!(price_cents: 1999, sku: 'apica', name: 'Apica Three Year Diary', category: diary, photo_url: 'https://i.imgur.com/YIKiPXQ.jpg')
+Product.create!(price_cents: 299, sku: 'bande', name: 'Bande Masking Tape', category: tape, photo_url: 'https://images-na.ssl-images-amazon.com/images/I/61gkkdsMaIL._AC_SL1500_.jpg')
+Product.create!(price_cents: 999, sku: 'pilot', name: 'Pilot Vanishing Point Fountain Pen', category: pen, photo_url: 'https://themartinj.com/wp-content/uploads/2017/03/pen4.jpg')
+Product.create!(price_cents: 199, sku: 'mt', name: 'MT Casa', category: tape, photo_url: 'https://www.masking-tape.jp/en/common/images/lineup/special/casa/mtcasashade/sizemainimg2.jpg')
 puts 'Finished!'
