489b8b87786cfc60df084d437ac40a6ffb1ee6ff
Grant Holley <gvholley@gmail.com>
2020-06-24 21:34:14 +0900

    Changed Stripe API

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,9 +1,10 @@
 Rails.configuration.stripe = {
-  Stripe.setPublishableKey("pk_test_51Guw15LBsMZeYb07DWNl1rr2X575otShpRvR4wnRKbZ7xYLoGKKFe5hyro9OARbQdmAfhANnxXYx7s03HcMoPPBl00Q2NLihdQ");
+
   secret_key:      ENV['STRIPE_SECRET_KEY'],
   signing_secret:  ENV['STRIPE_WEBHOOK_SECRET_KEY']
 }
 
+Stripe.setPublishableKey("pk_test_51Guw15LBsMZeYb07DWNl1rr2X575otShpRvR4wnRKbZ7xYLoGKKFe5hyro9OARbQdmAfhANnxXYx7s03HcMoPPBl00Q2NLihdQ");
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
 StripeEvent.signing_secret = Rails.configuration.stripe[:signing_secret]
 
