b453cff5f1c58e23eed2e523cb90619897c3a6dc
Grant Holley <gvholley@gmail.com>
2020-06-24 21:32:31 +0900

    Fixing Stripe API typo

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,5 +1,5 @@
 Rails.configuration.stripe = {
-  Stripe.setPublishableKey("pk_test_51Guw15LBsMZeYb07DWNl1rr2X575otShpRvR4wnRKbZ7xYLoGKKFe5hyro9OARbQdmAfhANnxXYx7s03HcMoPPBl00Q2NLihdQ"),
+  Stripe.setPublishableKey("pk_test_51Guw15LBsMZeYb07DWNl1rr2X575otShpRvR4wnRKbZ7xYLoGKKFe5hyro9OARbQdmAfhANnxXYx7s03HcMoPPBl00Q2NLihdQ");
   secret_key:      ENV['STRIPE_SECRET_KEY'],
   signing_secret:  ENV['STRIPE_WEBHOOK_SECRET_KEY']
 }
